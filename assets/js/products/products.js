d21f76d039a98507c1aecd2bf2edb141f075b51c
PTC0436 <phatthanhchau123@gmail.com>
2026-02-07 18:27:36 +0700

    fix modal close in filter 4

--- assets/js/products/products.js
+++ assets/js/products/products.js
@@ -53,20 +53,20 @@ async function renderProductHomeFilter() {
       <div class="filter">
         <div class="filter__header">
           <h3 class="filter__heading">Bộ lọc</h3>
-          <button class="filter__clear filter__clear-all" data-bs-dismiss="modal" aria-label="Close">Clear all</button>
+          <button class="filter__clear filter__clear-all" data-bs-dismiss="modal">Clear all</button>
         </div>
 
         <div class="filter__label">
           <p class="filter__title">Thương hiệu</p>
-          <button class="filter__clear filter__clear-brand" data-bs-dismiss="modal" aria-label="Close">Clear</button>
+          <button class="filter__clear filter__clear-brand" data-bs-dismiss="modal">Clear</button>
         </div>
 
         <div class="filter__list">
-          <button class="filter__item filter__item-brand" data-brand="" data-bs-dismiss="modal" aria-label="Close">Tất cả</button>
+          <button class="filter__item filter__item-brand" data-brand="" data-bs-dismiss="modal">Tất cả</button>
           ${brandList
             .map(
               (brand) => `
-            <button class="filter__item filter__item-brand" data-brand="${brand}" data-bs-dismiss="modal" aria-label="Close">${brand}</button>
+            <button class="filter__item filter__item-brand" data-brand="${brand}" data-bs-dismiss="modal">${brand}</button>
           `,
             )
             .join("")}
@@ -74,15 +74,15 @@ async function renderProductHomeFilter() {
 
         <div class="filter__label">
           <p class="filter__title">Giới tính</p>
-          <button class="filter__clear filter__clear-gender" data-bs-dismiss="modal" aria-label="Close">Clear</button>
+          <button class="filter__clear filter__clear-gender" data-bs-dismiss="modal">Clear</button>
         </div>
 
         <div class="filter__list">
-          <button class="filter__item filter__item-gender" data-gender="" data-bs-dismiss="modal" aria-label="Close">Tất cả</button>
+          <button class="filter__item filter__item-gender" data-gender="" data-bs-dismiss="modal">Tất cả</button>
           ${genderList
             .map(
               (gender) => `
-            <button class="filter__item filter__item-gender" data-gender="${gender}" data-bs-dismiss="modal" aria-label="Close">${gender}</button>
+            <button class="filter__item filter__item-gender" data-gender="${gender}" data-bs-dismiss="modal">${gender}</button>
           `,
             )
             .join("")}
@@ -90,28 +90,28 @@ async function renderProductHomeFilter() {
 
         <div class="filter__label">
           <p class="filter__title">Giá</p>
-          <button class="filter__clear filter__clear-price" data-bs-dismiss="modal" aria-label="Close">Clear</button>
+          <button class="filter__clear filter__clear-price" data-bs-dismiss="modal">Clear</button>
         </div>
 
         <div class="filter__list">
-          <button class="filter__item filter__item-price" data-price-start="" data-price-end="" data-bs-dismiss="modal" aria-label="Close">Tất cả</button>
-          <button class="filter__item filter__item-price" data-price-start="0" data-price-end="1000000" data-bs-dismiss="modal" aria-label="Close">Dưới 1 triệu</button>
-          <button class="filter__item filter__item-price" data-price-start="1000000" data-price-end="2000000" data-bs-dismiss="modal" aria-label="Close">Từ 1 đến 2 triệu</button>
-          <button class="filter__item filter__item-price" data-price-start="2000000" data-price-end="4000000" data-bs-dismiss="modal" aria-label="Close">Từ 2 đến 4 triệu</button>
-          <button class="filter__item filter__item-price" data-price-start="4000000" data-price-end="Infinity" data-bs-dismiss="modal" aria-label="Close">Trên 4 triệu</button>
+          <button class="filter__item filter__item-price" data-price-start="" data-price-end="" data-bs-dismiss="modal">Tất cả</button>
+          <button class="filter__item filter__item-price" data-price-start="0" data-price-end="1000000" data-bs-dismiss="modal">Dưới 1 triệu</button>
+          <button class="filter__item filter__item-price" data-price-start="1000000" data-price-end="2000000" data-bs-dismiss="modal">Từ 1 đến 2 triệu</button>
+          <button class="filter__item filter__item-price" data-price-start="2000000" data-price-end="4000000" data-bs-dismiss="modal">Từ 2 đến 4 triệu</button>
+          <button class="filter__item filter__item-price" data-price-start="4000000" data-price-end="Infinity" data-bs-dismiss="modal">Trên 4 triệu</button>
         </div>
 
         <div class="filter__label">
           <p class="filter__title">Tags</p>
-          <button class="filter__clear filter__clear-tags" data-bs-dismiss="modal" aria-label="Close">Clear</button>
+          <button class="filter__clear filter__clear-tags" data-bs-dismiss="modal">Clear</button>
         </div>
 
         <div class="filter__list filter__list-tags">
-          <button class="filter__item filter__item-tag" data-tag="" data-bs-dismiss="modal" aria-label="Close">Tất cả</button>
+          <button class="filter__item filter__item-tag" data-tag="" data-bs-dismiss="modal">Tất cả</button>
           ${tagList
             .map(
               (tag) => `
-            <button class="filter__item filter__item-tag" data-tag="${tag}" data-bs-dismiss="modal" aria-label="Close"git >${tag}</button>
+            <button class="filter__item filter__item-tag" data-tag="${tag}" data-bs-dismiss="modal"git >${tag}</button>
           `,
             )
             .join("")}
